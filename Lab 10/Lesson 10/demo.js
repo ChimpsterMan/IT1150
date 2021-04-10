953e401eb4873711239a43ecfaca171d2c4ce906
ChimpsterMan <emnilsson007@gmail.com>
2021-04-10 11:54:58 -0400

    lab 10

---
+++ Lab 10/Lesson 10/demo.js
@@ -0,0 +1,85 @@
+//Javascript is case sensitive
+
+/*Javascript statement end in a semicolon
+Javascript comments can span single or multiple lines*/
+
+
+console.log("console log - hello world");//log in developer tools
+var globalX = 100;//global scope
+
+function hello() //function definition
+{
+    alert("alert - hello world!");//popup window    
+}
+
+function add()//function definition
+{
+    var x = 3;
+    var y = 5;
+    var sum = x + y;
+    alert("Sum is " + sum);
+}
+
+function substract()
+{
+    var a = 1000;
+    var b = 100;
+    var diff = a - b;
+    alert("Difference is" + diff);
+}
+
+//Homework Lab- Complete Multiplication and Division functionality
+
+function increment()
+{ 
+    var y = 10;//local scope - resets y everytime function is called
+    globalX = globalX+10;//can add any number to itself
+    //y = y+10;
+    y+=10;//same as y = y+10;
+    alert("x="+globalX + "y="+y); 
+
+}
+
+function compare()
+{
+    var charlie = 120;
+    var john = 120;
+    
+    //conditional if statement
+    
+    if (charlie > john)
+    {
+        alert("Charlie is taller");
+    }
+    else if( charlie == john)//"=" is assignment; "==" is comparison
+    {
+        alert("Charlie and John are of the same height");
+    }
+    else
+    {
+        alert("John is taller");
+    }
+}
+
+ // homework assignment
+ function func1(){
+     alert(25*42);
+ }
+
+ function func2(){
+     alert(48/12);
+ }
+
+ function func3(){
+    var sum = 25+47+98+46+52;
+
+     alert(sum/5);
+ }
+
+ function func4(){
+     alert("No");
+ }
+
+ function func5(){
+     alert("No");
+ }
